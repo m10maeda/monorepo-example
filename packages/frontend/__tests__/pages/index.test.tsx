0dafbe932e11faf5450ad8ed83458fb4026a28ad
m10maeda <mio.maeda.kari@gmail.com>
2021-10-14 11:34:56 +0900

    chore: add success tests

--- packages/frontend/__tests__/pages/index.test.tsx
+++ packages/frontend/__tests__/pages/index.test.tsx
@@ -8,3 +8,7 @@ test('renders learn Next.js link', () => {
   const element = screen.getByText(/Next.js!/i);
   expect(element).toBeInTheDocument();
 });
+
+it('success test', () => {
+  expect(true).toBeTruthy();
+});
